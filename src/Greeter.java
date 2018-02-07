a56da370a7ea9f38fd7e5e99c3416557157d1e46
Unknown <gracehughes317@gmail.com>
2018-02-07 10:48:28 -0600

    Greeter class coded

--- src/Greeter.java
+++ src/Greeter.java
@@ -1,4 +1,27 @@
 
 public class Greeter {
 
+	private String name;
+	private int age;
+	
+	public Greeter()
+	{
+		name = "John Smith";
+		age = 20;
+	}
+
+	public Greeter(String xname, int xage)
+	{
+	name = xname;
+	age = xage;
+	}
+	
+	public String toString()
+	{
+		return "Name: "+ name + " Age: " + age;
+		
+	}
+	
+	
+	
 }
