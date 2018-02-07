78363e622dcbfbe96c536b2a89a452b70da10b6a
Unknown <gracehughes317@gmail.com>
2018-02-07 10:50:38 -0600

    GreeterTester coded

--- src/GreeterTest.java
+++ src/GreeterTest.java
@@ -4,6 +4,17 @@ public class GreeterTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		
+		Greeter bob = new Greeter("Bob",52);
+		System.out.println(bob.toString());
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
